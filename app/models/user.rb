8511aa69170692693c42afd3ae13850b893be2a1
Vincent Langat <kimutailangat033@gmail.com>
2022-10-13 19:50:16 +0300

    validates user, has secure password, validate username,email and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    validates :username, uniqueness: true, presence: true, length: {in: 4..25}
+    validates :email, uniqueness: true, presence: true,format: {with: /\A(?<username>[^@\s]+)@((?<domain_name>[-a-z0-9]+)\.(?<domain>[a-z]{2,}))\z/i}
+    validates :password, length: {in: 6..250}
 end
