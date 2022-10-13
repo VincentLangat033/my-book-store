8de757194d560b2725658689b2c92d080e62e1e4
Vincent Langat <kimutailangat033@gmail.com>
2022-10-13 22:20:32 +0300

    validate rating, presence of a numerical and establish relationship

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,2 +1,8 @@
 class Rating < ApplicationRecord
+
+    validates :rating, numericality: { in: 1..5 }, presence: true
+    validates :book_id, presence: true
+
+    belongs_to :user
+    belongs_to :book
 end
