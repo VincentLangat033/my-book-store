ee19d6e57ec8a0536ff884d0696208f6ffd50288
Vincent Langat <kimutailangat033@gmail.com>
2022-10-13 22:16:06 +0300

    add all resource routers

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,24 @@
 Rails.application.routes.draw do
 
-    get '/hello', to: 'application#hello_world'
-    get '/home' , to: 'application#home'
+  resources :ratings
+  resources :bookshelves
+  resources :comments
+  resources :books # , only: [:create, ]
 
-    resources :users, only: [:update, :destroy]
+  resources :users, only: [:update, :destroy]
 
-    get '*path',
-      to: 'fallback#index',
-      constraints: ->(req) { !req.xhr? && req.format.html? }
+  get "/me", to: "users#show"
+  post "/signup", to: "users#create"
+
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
+  get "/myratings", to: "ratings#my_ratings"
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+ 
+  # Routing logic: fallback requests for React Router.
+  # Leave this here to help deploy your app later!
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
