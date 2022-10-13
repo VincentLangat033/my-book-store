beb51032adf4cff9fa91b78a0c1592ff728a4b8c
Vincent Langat <kimutailangat033@gmail.com>
2022-10-13 22:22:07 +0300

    book model validations and association setting-up

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,15 @@
 class Book < ApplicationRecord
+    has_secure_password
+
+    validates :username, uniqueness: true, presence: true, length: {in: 4..25}
+    validates :email, uniqueness: true, presence: true,format: {with: /\A(?<username>[^@\s]+)@((?<domain_name>[-a-z0-9]+)\.(?<domain>[a-z]{2,}))\z/i}
+    validates :password, length: {in: 6..250}
+    
+  
     has_many :bookshelves
     has_many :comments
     has_many :ratings
-    has_many :user_comments, through: :comments, source: :user
-    has_many :user_bookshelves, through: :bookshelves, source: :user
-    has_many :user_ratings, through: :ratings, source: :user
+    has_many :commented_books, through: :comments, source: :book
+    has_many :shelved_books, through: :bookshelves, source: :book
+    has_many :rated_books, through: :ratings, source: :book
 end
